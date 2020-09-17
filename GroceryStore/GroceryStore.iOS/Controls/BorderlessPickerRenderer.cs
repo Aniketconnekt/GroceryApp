287b621d6ae8ea66e69e36e4d3164937876e3467
connektind <viky@connekt.in>
2020-09-17 19:14:57 +0530

    new commit for static ui

---
+++ GroceryStore/GroceryStore.iOS/Controls/BorderlessPickerRenderer.cs
@@ -0,0 +1,35 @@
+﻿using GroceryStore.Controls;
+using GroceryStore.Droid.Controls;
+using System.Runtime.Remoting.Contexts;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.iOS;
+
+[assembly: ExportRenderer(typeof(BorderlessPicker), typeof(BorderlessPickerRenderer))]
+namespace GroceryStore.Droid.Controls
+{
+    public class BorderlessPickerRenderer : PickerRenderer
+    {
+        //public BorderlessPickerRenderer(Context context) : base(context)
+        //{
+        //}
+        public BorderlessPickerRenderer(Context context) 
+        {
+        }
+        public static void Init() { }
+        protected override void OnElementChanged(ElementChangedEventArgs<Picker> e)
+        {
+            base.OnElementChanged(e);
+            if (e.OldElement == null)
+            {
+                Control.Background = null;
+
+                //var layoutParams = new MarginLayoutParams(Control.LayoutParameters);
+                //layoutParams.SetMargins(0, 0, 0, 0);
+                //LayoutParameters = layoutParams;
+                //Control.LayoutParameters = layoutParams;
+                //Control.SetPadding(0, 0, 0, 0);
+                //SetPadding(0, 0, 0, 0);
+            }
+        }
+    }
+}
